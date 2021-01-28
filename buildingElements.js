3a85321dea25283574c6fbdcbee4ffaf30776e92
jrandersen-ga <andersen.ja@gmail.com>
2021-01-28 17:02:35 -0500

    updating addition method in Walls

--- buildingElements.js
+++ buildingElements.js
@@ -17,18 +17,18 @@ class Wall {
     matThickness(width){
       let sum = 0;
       for (let i = 0; i < this.construction.length; i++) {
-        if (this.construction.percentDistribution === 100) {
-            sum += this.construction[i].thickness;
-            console.log('100', sum)
-        }else {
-            if (this.construction[i].thickness ===  this.construction[i+1].thickness) {
-                sum += this.construction[i].thickness;
-                console.log('less than 100', sum)
-                continue;
-            } 
+        const element = this.construction[i];
+        const nextElement = this.construction[i+1];
+        console.log(nextElement)
+        if (element.percentDistribution === 100) {
+            sum += element.thickness;
+            console.log('100', sum);
+        }else{
+            sum += element.thickness;
+            console.log('less than 100', sum);
         }
       }
-      return sum
+      return sum;
     }    
 };
 
